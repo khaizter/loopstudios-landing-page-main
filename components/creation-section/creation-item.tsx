4be33c07fc5bed8dc359f07ab30702e75b4c75b3
khaizter <vashh.dev@gmail.com>
2024-06-25 21:18:40 +0800

    Creation item black gradient overlay

--- components/creation-section/creation-item.tsx
+++ components/creation-section/creation-item.tsx
@@ -12,6 +12,7 @@ const CreationItem: React.FC<CreationItemProps> = (props) => {
   return (
     <li className="p-6 pt-10 bg-blue-300 relative text-xl text-neutral-50">
       <Image src={image} alt={label} fill />
+      <div className="absolute inset-0 bg-gradient-to-r from-stone-900 to-transparent"></div>
       <div className="relative z-10 uppercase max-w-[6.5rem]">{label}</div>
     </li>
   );
